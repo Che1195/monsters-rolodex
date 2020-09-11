354ff9c3730aba7d302b36325470d3ffbd0292a2
Che Ndumbi <che.ndumbi@gmail.com>
2020-09-11 10:18:22 -0400

    Updates

---
+++ precache-manifest.41f13669e1a0968e0ceff66d05efb22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b15a6a44874a92b108840cc8476e0d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ad608360b30970dcf5ff",
+    "url": "/monsters-rolodex/static/css/main.ad0f73ad.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad608360b30970dcf5ff",
+    "url": "/monsters-rolodex/static/js/main.6542dcba.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
